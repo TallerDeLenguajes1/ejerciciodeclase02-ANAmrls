0b54f113934a03f88d41d29f4a479e013d1fd0c5
Ana Lucia Morales <analuciamrls@gmail.com>
2021-09-08 17:59:29 -0300

    primer commit

---
+++ CuentasBancarias/CuentasBancarias/CuentaCorrientePesos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentasBancarias
+{
+    public enum TipoExtraccion
+    {
+        CajeroHumano = 1,
+        CajeroAutomatico = 2
+    }
+
+    class CuentaCorrientePesos
+    {
+        decimal fondo;
+
+        public decimal Fondo { get => fondo; set => fondo = value; }
+
+        public void Insercion(int monto)
+        {
+            Fondo += monto;
+        }
+
+        public decimal Extraccion(int monto, TipoExtraccion tipo)
+        {
+            if (monto <= Fondo + 5000)
+            {
+                if (tipo == TipoExtraccion.CajeroAutomatico)
+                {
+                    return monto;
+                }
+                else
+                {
+                    if (monto <= 20000)
+                    {
+                        return monto;
+                    }
+                    else
+                    {
+                        Console.WriteLine("El monto máximo de extraccion por cajero automatico es de $20.000");
+                        return 0;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fondos insuficientes");
+                return 0;
+            }
+            
+        }
+    }
+}
---
+++ CuentasBancarias/CuentasBancarias/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CuentasBancarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
